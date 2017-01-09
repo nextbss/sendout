2696c29b27a38abbcda7c39065adaf41fd6cde2b
Sergio Maziano <sergio.maziano@gmail.com>
2017-01-09 23:23:30 +0100

    Changed the default value for debug to nil

--- lib/sendout.rb
+++ lib/sendout.rb
@@ -12,7 +12,7 @@ module Sendout
   end
   
   # send sms: dest is always an array of numbers and the message a string
-  def self.send_sms(numbers=[], message, debug=false)
+  def self.send_sms(numbers=[], message, debug = nil)
     # fields from API
     data = {"para" => numbers, "sms" => message, "debug" => debug}
 
