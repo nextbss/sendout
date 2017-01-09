3c316328f8e693b1df2ed29ad62c392a79b41f42
Sergio Maziano <sergio.maziano@gmail.com>
2017-01-09 23:50:42 +0100

    Changes the optional position

--- lib/sendout.rb
+++ lib/sendout.rb
@@ -12,11 +12,9 @@ module Sendout
   end
   
   # send sms: dest is always an array of numbers and the message a string
-  def self.send_sms(numbers=[], message, test)
-    # default for test
-    test ||= false
+  def self.send_sms(numbers=[], debug={}, message)
     # fields from API
-    data = {"para" => numbers, "sms" => message, "debug" => test}
+    data = {"para" => numbers, "sms" => message, "debug" => debug}
 
     # merge the body content
     content = {}
