cff03d444ebd3032f3b25cb66f42ee3516429d7b
Sergio Maziano <sergio.maziano@gmail.com>
2017-01-09 23:38:15 +0100

    Removed the option for default value for debug

--- lib/sendout.rb
+++ lib/sendout.rb
@@ -12,7 +12,9 @@ module Sendout
   end
   
   # send sms: dest is always an array of numbers and the message a string
-  def self.send_sms(numbers=[], message, test=[])
+  def self.send_sms(numbers=[], message, test)
+    # default for test
+    test ||= false
     # fields from API
     data = {"para" => numbers, "sms" => message, "debug" => test}
 
